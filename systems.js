13ab97793f466f2c6bb5660e1c0f700a14f35589
ravi <rafael.vitor@jusbrasil.com.br>
2018-12-27 14:37:43 -0300

    Add continuos movement

--- systems.js
+++ systems.js
@@ -3,57 +3,55 @@ import { Dimensions } from 'react-native';
 const MoveFinger = (entities, props) => {
   const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
 
-  let finger = entities[1];
   let arrow = entities[2];
   let square = entities[3];
 
+  let direction = arrow.direction;
+
   props.touches.filter(t => t.type === "move").forEach(t => {
-    let direction;
 
-    if (finger && finger.position) {
-      finger.position = [ finger.position[0] + t.delta.pageX, finger.position[1] + t.delta.pageY ];
+    const { pageX, pageY } = t.delta;
 
-      const { pageX, pageY } = t.delta;
-
-
-      if (Math.abs(pageX) > Math.abs(pageY)) {
-        if (pageX > 0) { direction = 'right' }
-        else { direction = 'left' }
-      } else {
-        if (pageY > 0) { direction = 'down' }
-        else { direction = 'up'; }
-      }
+    if (Math.abs(pageX) > Math.abs(pageY)) {
+      if (pageX > 0) { direction = 'right' }
+      else { direction = 'left' }
+    } else {
+      if (pageY > 0) { direction = 'down' }
+      else { direction = 'up'; }
     }
+  });
 
+  const movements = {
+    up: -10,
+    right: 10,
+    down: 10,
+    left: -10,
+  };
+
+  const newPosition = [...square.position]
+
+  if (square && square.position) {
+    const delta = props.time.delta/16
     const movements = {
-      up: -10,
-      right: 10,
-      down: 10,
-      left: -10,
+      up: square.position[1] - delta,
+      right: square.position[0] + delta,
+      left: square.position[0] - delta,
+      down: square.position[1] + delta,
     };
 
-    const newPosition = [...square.position]
-
-    if (square && square.position) {
-      if (direction === 'left' || direction === 'right') {
-        newValue = square.position[0] + movements[direction];
-        newPosition[0] = newValue >= screenWidth || newValue < 0 ? screenWidth/2 : newValue
-      }
-      if (direction === 'up' || direction === 'down') {
-        newValue = square.position[1] + movements[direction];
-        newPosition[1] = newValue >= screenHeight || newValue < 0 ? screenHeight/2 : newValue
-      }
+    console.log('tick', direction);
+    if (direction === 'left' || direction === 'right') {
+      newValue = movements[direction];
+      newPosition[0] = newValue >= screenWidth || newValue < 0 ? screenWidth/2 : newValue
     }
-
-    square.position = newPosition
-    arrow.direction = direction;
-  });
-
-  props.touches.filter(t => t.type === "end").forEach(t => {
-    if (finger && finger.position) {
-      finger.position = [props.screen.width/2, props.screen.height/2];
+    if (direction === 'up' || direction === 'down') {
+      newValue = movements[direction];
+      newPosition[1] = newValue >= screenHeight || newValue < 0 ? screenHeight/2 : newValue
     }
-  });
+  }
+
+  square.position = newPosition
+  arrow.direction = direction;
 
   return entities;
 };
