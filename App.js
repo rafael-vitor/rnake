42af99c320249ca673f026cb8a8e7cdcc35984a3
ravi <rafael.vitor@jusbrasil.com.br>
2018-12-27 13:51:02 -0300

    Improve swipe detection and square movement

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { AppRegistry, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StatusBar, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { GameEngine } from "react-native-game-engine";
-import { Finger } from "./renderers";
+import { Finger } from "./finger";
+import { Square } from "./square";
 import { MoveFinger } from "./systems";
 import { Arrow } from './arrow';
 
 export default class App extends React.Component {
   render() {
+    const { height, width } = Dimensions.get('window');
+    // console.log({ height, width })
+
     return (
-      // <View style={styles.container}>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      // </View>
       <GameEngine
         style={styles.container}
         systems={[MoveFinger]}
         entities={{
-          1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
-          2: { direction: 'left', renderer: <Arrow />}, //-- Notice that each entity has a unique id (required)
+          1: { position: [40,  200], renderer: <Finger />},
+          2: { direction: '', renderer: <Arrow />},
+          3: { position: [width/2, height/2], renderer: <Square />},
         }}>
 
         <StatusBar hidden={false} />
@@ -30,7 +32,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
